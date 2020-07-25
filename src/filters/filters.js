e06d7595a047260f4dd945a381debf083fef5a4f
raulpy271 <56479285+raulpy271@users.noreply.github.com>
2020-07-25 12:20:21 -0300

    Update filters.js
    adding `contrast` and `threshold` filters

--- src/filters/filters.js
+++ src/filters/filters.js
@@ -1,45 +1,90 @@
+import {halfOfMaxValueOfChannel,
+  maxValueOfOneChannel, getMean} 
+  from '/src/filters/tools.js'
+export {filters}
 
-export var filters = {
 
-  grayscale: function() {
+var filters = {}
+filters.grayscale = function() {
 
-    function getMean(list) {
-      return (list.reduce((sum, val) => (sum += val))) / list.length
-    }
-
-    function pixelToGrayscale(pixel) {
-      var alfaChannelValue = pixel.pop()
-      var pixelWithoutAlfaChannel = pixel
-      var mean = getMean(pixelWithoutAlfaChannel )
-      var newPixel = this.mapPixel(
-        function(trash) {return mean}, pixelWithoutAlfaChannel
-      )
-      newPixel.push(alfaChannelValue)
-      return newPixel
-    }
-
-    function callPixelToGrayscaleWithCordinate(x, y) {
-      var newPixel =  pixelToGrayscale.call(this, this.getPixel(x, y))
-      return newPixel
-    }
-
-    this.mapAllCanvas(callPixelToGrayscaleWithCordinate)
-  },
-
-  brigthness: function(intensity) {
-
-    function brigthnessInOnePixel(pixel, intensity) {
-      return this.applyFunctionToPixelWithoutApplyToAlfaChannel(
-        function(channelValue) {
-          return channelValue + intensity
-        }, pixel)
-    }
-
-    function brigthnessInOnePixelWithCordinate(x, y) {
-      var newPixel =  brigthnessInOnePixel.call(this, this.getPixel(x, y), intensity)
-      return newPixel
-    }
-
-    this.mapAllCanvas(brigthnessInOnePixelWithCordinate)
+  function pixelToGrayscale(pixel) {
+    var alfaChannelValue = pixel.pop()
+    var pixelWithoutAlfaChannel = pixel
+    var mean = getMean(pixelWithoutAlfaChannel )
+    var newPixel = this.mapPixel(
+      function(trash) {return mean}, pixelWithoutAlfaChannel
+    )
+    newPixel.push(alfaChannelValue)
+    return newPixel
   }
+
+  function callPixelToGrayscaleWithCordinate(x, y) {
+    var newPixel =  pixelToGrayscale.call(this, this.getPixel(x, y))
+    return newPixel
+  }
+
+  this.mapAllCanvas(callPixelToGrayscaleWithCordinate)
+}
+
+
+filters.brigthness = function(intensity) {
+
+  function brigthnessInOnePixel(pixel, intensity) {
+    return this.applyFunctionToPixelWithoutApplyToAlfaChannel(
+      function(channelValue) {
+        return channelValue + intensity
+      }, pixel)
+  }
+
+  function brigthnessInOnePixelWithCordinate(x, y) {
+    var newPixel =
+      brigthnessInOnePixel.call(this, this.getPixel(x, y), intensity)
+    return newPixel
+  }
+
+  this.mapAllCanvas(brigthnessInOnePixelWithCordinate)
+}
+
+
+filters.contrast = function(intensity) {
+  intensity = (intensity / 100) + 1
+
+  function contrastInOnePixel(pixel, intensity) {
+    return this.
+    applyFunctionToPixelWithoutApplyToAlfaChannel(
+      function(channelValue) {
+        return channelValue * intensity
+      }, pixel)
+  }
+
+  function contrastInOnePixelWithCordinate(x, y) {
+    var newPixel =
+      contrastInOnePixel.call(this, this.getPixel(x, y), intensity)
+    return newPixel
+  }
+
+  this.mapAllCanvas(contrastInOnePixelWithCordinate)
+}
+
+
+filters.threshold = function(limitValue = halfOfMaxValueOfChannel ) {
+
+  function thresholdInOnePixel(pixel, limit) {
+    var alfaChannelValue = pixel.pop()
+    var pixelWithoutAlfaChannel = pixel
+    var mean = getMean(pixelWithoutAlfaChannel )
+    var newPixel = mean > limit ? 
+      [ maxValueOfOneChannel, maxValueOfOneChannel,
+        maxValueOfOneChannel, alfaChannelValue] 
+      : [0, 0, 0, alfaChannelValue] 
+    return newPixel
+  }
+
+  function thresholdInOnePixelWithCordinate(x, y) {
+    var newPixel =  
+      thresholdInOnePixel.call(this, this.getPixel(x, y), limitValue)
+    return newPixel
+  }
+  
+  this.mapAllCanvas(thresholdInOnePixelWithCordinate)
 }
